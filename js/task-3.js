a104567a62102ae848d9a272d650bb1982626605
Maksim Peskov <hasagiluv@gmail.com>
2025-02-24 21:20:30 +0200

    update js task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = users =>
-  users.toSorted((a, b) => b.friends - a.friends);
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
